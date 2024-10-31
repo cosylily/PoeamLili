06aadc4ab4b5e93883ffea595a21400c17d10476
cosylily <syafiqah.dayana1205@gmail.com>
2024-10-31 22:56:20 +0000

    Added js function to generate poems

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-function generatePoem() {
+function displayPoem(response) {
+  let poemText = document.querySelector(".poem");
+  new Typewriter(poemText, {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 25,
+    cursor: "",
+  });
+}
+function generatePoem(event) {
   event.preventDefault();
   let poemText = document.querySelector(".poem");
-
+  let searchInput = document.querySelector(".search");
+  let apiKey = "3caeb6a7a0144t0bff6oc0e38972db67";
+  let prompt = `${searchInput.value}`;
+  let context =
+    "A great and knowledgeable poet who can make good poems. Please generate appropriate and good, short 4-line poem  in basic HTML. Please follow user's instruction. Present the poem without the word html";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   new Typewriter(poemText, {
     strings: "Generating Poem...",
     autoStart: true,
     delay: 25,
     cursor: "",
   });
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let title = document.querySelector(".poemTitle");
